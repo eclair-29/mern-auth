74f1f13164d6b36b32f27e921554cda1c8f676c4
Migz De Chavez <dmgzky@gmail.com>
2019-03-27 13:09:26 +0800

    update store location

--- client/src/index.js
+++ client/src/index.js
@@ -2,28 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
-import thunk from "redux-thunk";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import rootReducer, { authActions } from "./store";
-
-// Redux store
-// eslint-disable-next-line no-underscore-dangle
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
-);
-
-// if there is a proper token in user browser/local storage
-// dispatch an action that sets isAuthenticated to true
-const token = localStorage.getItem("token");
-
-if (token) {
-  store.dispatch(authActions.autoAuth());
-}
+import store from "./store";
 
 ReactDOM.render(
   <Provider store={store}>
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,11 +1,29 @@
-import { combineReducers } from "redux";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import thunk from "redux-thunk";
 import { authReducer, errorReducer } from "./reducers";
+import { authActions } from "./actions";
 
 const rootReducer = combineReducers({
   auth: authReducer,
   error: errorReducer
 });
 
-export default rootReducer;
+// Redux store
+// eslint-disable-next-line no-underscore-dangle
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+// if there is a proper token in user browser/local storage
+// dispatch an action that sets isAuthenticated to true
+const token = localStorage.getItem("token");
+
+if (token) {
+  store.dispatch(authActions.autoAuth());
+}
+
+export default store;
 export { default as storeOperations } from "./operations";
 export { authActions, errorActions } from "./actions";
